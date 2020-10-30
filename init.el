c3ccc7d1800594ef8a145e4bd4df729e6f2ec340
Kavin Desi Valli <kavinvalli@gmail.com>
2020-10-30 12:18:41 +0530

    auto changes custom vars

--- init.el
+++ init.el
@@ -689,3 +689,16 @@
 (use-package cricbuzz)
 
 (+ 50 100)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(cricbuzz ivy-youtube counsel-spotify org-gcal ivy-pass lorem-ipsum forge evil-magit magit counsel-projectile projectile company-box company lsp-python-ms scss-mode skewer-mode impatient-mode web-mode prettier-js js2-mode typescript-mode lsp-treemacs lsp-ivy lsp-ui lsp-mode speed-type counsel-osx-app org-alert org-make-toc org-special-block-extras visual-fill-column org-plus-contrib org-bullets hydra evil-nerd-commenter yasnippet emmet-mode autopair helpful which-key counsel ivy-rich ivy command-log-mode evil-collection evil general rainbow-delimiters doom-themes doom-modeline all-the-icons use-package exec-path-from-shell autothemer)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
